0dbef280ca684a907338235cb6afe226b4337d0f
UncannyNyl <mysteriousmun21@gmail.com>
2026-05-05 21:10:19 +0800

    Database connection

--- config/database.php
+++ config/database.php
@@ -7,13 +7,15 @@ $password = '';
 
 try {
     // 1. The DSN (Data Source Name) tells PDO where to connect.
-    $dsn = "mysql:host=$host;dbname=$dbname; charset=utf8mb4";
+    $dsn = "mysql:host=$host;port=3307;dbname=$dbname;charset=utf8mb4";
     
     // 2. Instantiate the PDO object.
     $pdo = new PDO($dsn, $username, $password);
     
     // 3. Set error mode to Exception so it throws errors we can catch.
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    return $pdo ;
     
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
